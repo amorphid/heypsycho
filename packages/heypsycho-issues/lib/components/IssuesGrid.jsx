8b20fc9c233b55071db296650a074d66e0a4578e
zachalbert <zachalbert@gmail.com>
2016-12-11 00:47:10 -0800

    Turn issues into cards

--- packages/heypsycho-issues/lib/components/IssuesGrid.jsx
+++ packages/heypsycho-issues/lib/components/IssuesGrid.jsx
@@ -5,20 +5,20 @@ import { FormattedMessage, FormattedRelative } from 'react-intl';
 import { Link } from 'react-router';
 
 const IssuesGrid = ({results, currentUser, hasMore, ready, count, totalCount, loadMore, showHeader = true}) => {
-  
+
   if (!!results.length) {
-    
+
     return (
-      <div className="posts-list">
+      <div className="posts-list container">
         {showHeader ? <Telescope.components.PostsListHeader /> : null}
-        <div className="posts-list-content posts-grid">
+        <div className="posts-list-content posts-grid row">
           {results.map(post => <Telescope.components.IssuesGridItem post={post} key={post._id}/>)}
         </div>
         {hasMore ? (ready ? <Telescope.components.PostsLoadMore loadMore={loadMore} count={count} totalCount={totalCount} /> : <Telescope.components.PostsLoading/>) : <Telescope.components.PostsNoMore/>}
       </div>
     )
   } else if (!ready) {
-    
+
     return (
       <div className="posts-list">
         {showHeader ? <Telescope.components.PostsListHeader /> : null}
@@ -28,7 +28,7 @@ const IssuesGrid = ({results, currentUser, hasMore, ready, count, totalCount, lo
       </div>
     )
   } else {
-    
+
     return (
       <div className="posts-list posts-grid">
         {showHeader ? <Telescope.components.PostsListHeader /> : null}
@@ -36,9 +36,9 @@ const IssuesGrid = ({results, currentUser, hasMore, ready, count, totalCount, lo
           <Telescope.components.PostsNoResults/>
         </div>
       </div>
-    )  
+    )
   }
-  
+
 };
 
 export default IssuesGrid;
