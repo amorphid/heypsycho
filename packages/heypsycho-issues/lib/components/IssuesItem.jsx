1bd78ddcb769c106339e096217b66588f21a0f33
zachalbert <zachalbert@gmail.com>
2016-12-11 13:17:44 -0800

    Add missing containers around rows

--- packages/heypsycho-issues/lib/components/IssuesItem.jsx
+++ packages/heypsycho-issues/lib/components/IssuesItem.jsx
@@ -41,73 +41,78 @@ class IssuesItem extends Telescope.components.PostsItem {
               {this.renderCategories()}
             </h1>
 
-            <div className="row center-xs lead-single">
-              <div className="col-xs-11">
-                <div className="posts-item-consequences">
-                  <div className="row">
-                    <div className="col-xs-6 posts-item-consequences--left lead-single">
-                      <h3 className="text-center lead-single">Consequences</h3>
-                      <p className="text-center lead-single">
-                        Until we hear from the company, we're going to take collective
-                        action to ensure our voices are heard. Join us.
-                      </p>
-                      <ul>
-                        <li><strong>100 stories</strong>: We'll issue a press release, designed to get the attention of traditional media outlets</li>
-                        <li><strong>200 stories</strong>: We'll build a facebook ad campaign so people can hear your stories directly</li>
-                        <li><strong>300 stories</strong>: We'll hire a lawyer to begin pursuing legal action</li>
-                      </ul>
-                    </div>
-                    <div className="col-xs-6 posts-item-consequences--right lead-single">
-                      <h3 className="text-center lead-single">Response</h3>
-                      <p className="text-center lead-single">No official response yet&hellip;</p>
-                      <p className="text-center lead-single">
-                        Do you represent the organization?
-                        <a className="btn btn-secondary" href="#">Respond</a>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-xs">
-                      <div className="amplify">
-                        <h3 className="text-center lead-single">Do you have a story to share,<br />or can you help us amplify our voices?</h3>
-                        <p className="text-center">
-                          <a className="btn btn-secondary" href="#">Join us</a>
+            <div className="container">
+              <div className="row center-xs lead-single">
+                <div className="col-xs-11">
+                  <div className="posts-item-consequences">
+                    <div className="row">
+                      <div className="col-xs-6 posts-item-consequences--left lead-single">
+                        <h3 className="text-center lead-single">Consequences</h3>
+                        <p className="text-center lead-single">
+                          Until we hear from the company, we're going to take collective
+                          action to ensure our voices are heard. Join us.
                         </p>
+                        <ul>
+                          <li><strong>100 stories</strong>: We'll issue a press release, designed to get the attention of traditional media outlets</li>
+                          <li><strong>200 stories</strong>: We'll build a facebook ad campaign so people can hear your stories directly</li>
+                          <li><strong>300 stories</strong>: We'll hire a lawyer to begin pursuing legal action</li>
+                        </ul>
+                      </div>
+                      <div className="col-xs-6 posts-item-consequences--right lead-single">
+                        <h3 className="text-center lead-single">Response</h3>
+                        <p className="text-center lead-single">No official response yet&hellip;</p>
+                        <p className="text-center lead-single">
+                          Do you represent the organization?
+                          <a className="btn btn-secondary" href="#">Respond</a>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-xs">
+                        <div className="amplify">
+                          <h3 className="text-center lead-single">Do you have a story to share,<br />or can you help us amplify our voices?</h3>
+                          <p className="text-center">
+                            <a className="btn btn-secondary" href="#">Join us</a>
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="row center-xs">
-              <div className="col-sm-10">
+              <div className="row center-xs">
+                <div className="col-sm-10">
 
-                {post.body}
+                  <p>
+                    {post.body}
+                  </p>
 
 
-                {/* Post meta stuff
-                <div className="posts-item-meta">
-                  {post.user? <div className="posts-item-user"><Telescope.components.UsersAvatar user={post.user} size="small"/><Telescope.components.UsersName user={post.user}/></div> : null}
-                  <div className="posts-item-date"><FormattedRelative value={post.postedAt}/></div>
-                  <div className="posts-item-comments">
-                    <Link to={Posts.getPageUrl(post)}>
-                      <FormattedMessage id="comments.count" values={{count: post.commentCount}}/>
-                    </Link>
+                  {/* Post meta stuff
+                  <div className="posts-item-meta">
+                    {post.user? <div className="posts-item-user"><Telescope.components.UsersAvatar user={post.user} size="small"/><Telescope.components.UsersName user={post.user}/></div> : null}
+                    <div className="posts-item-date"><FormattedRelative value={post.postedAt}/></div>
+                    <div className="posts-item-comments">
+                      <Link to={Posts.getPageUrl(post)}>
+                        <FormattedMessage id="comments.count" values={{count: post.commentCount}}/>
+                      </Link>
+                    </div>
+                    {this.context.currentUser && this.context.currentUser.isAdmin ? <Telescope.components.PostsStats post={post} /> : null}
+                    {this.renderActions()}
                   </div>
-                  {this.context.currentUser && this.context.currentUser.isAdmin ? <Telescope.components.PostsStats post={post} /> : null}
-                  {this.renderActions()}
+
+                  {this.renderCommenters()}
+                  */}
+
+                  <div className="posts-item-vote">
+                    {/* Create a custom vote component that replaces the up arrow with an eye icon for watching */}
+                    <Telescope.components.Vote post={post} />
+                  </div>
+
                 </div>
-
-                {this.renderCommenters()}
-                */}
-
-                <div className="posts-item-vote">
-                  {/* Create a custom vote component that replaces the up arrow with an eye icon for watching */}
-                  <Telescope.components.Vote post={post} />
-                </div>
-
               </div>
+
             </div>
 
           </div>
--- packages/heypsycho-issues/lib/components/IssuesPage.jsx
+++ packages/heypsycho-issues/lib/components/IssuesPage.jsx
@@ -15,56 +15,60 @@ const IssuesPage = ({document, currentUser}) => {
 
   const params = {};
   const {selector, options} = Posts.parameters.get(params);
-  
+
   const praisesSelector = Object.assign({}, selector);
   praisesSelector['categories'] = praiseCategoryId;
-  
+
   const grievancesSelector = Object.assign({}, selector);
   grievancesSelector['categories'] = grievancesCategoryId;
-  
+
   return (
     <div className="posts-page">
 
-      <div className="row">
-        <Telescope.components.HeadTags url={Posts.getLink(post)} title={post.title} image={post.thumbnailUrl} />
+      <div className="container">
+        <div className="row">
+          <Telescope.components.HeadTags url={Posts.getLink(post)} title={post.title} image={post.thumbnailUrl} />
+        </div>
       </div>
 
       <Telescope.components.IssuesItem post={post}/>
 
-      <div className="stories-lists row">
+      <div className="container">
+        <div className="stories-lists row">
 
-        <div className="col-sm-6">
-          <Telescope.components.IssuesStoriesHeader type="Praises"/>
-          <ListContainer
-            collection={Posts}
-            publication="posts.list"
-            selector={praisesSelector}
-            options={options}
-            terms={params}
-            joins={Posts.getJoins()}
-            component={Telescope.components.IssuesStoriesList}
-            cacheSubscription={true}
-            listId="praise"
-            limit={Telescope.settings.get("postsPerPage", 10)}
-          />
+          <div className="col-sm-6">
+            <Telescope.components.IssuesStoriesHeader type="Praises"/>
+            <ListContainer
+              collection={Posts}
+              publication="posts.list"
+              selector={praisesSelector}
+              options={options}
+              terms={params}
+              joins={Posts.getJoins()}
+              component={Telescope.components.IssuesStoriesList}
+              cacheSubscription={true}
+              listId="praise"
+              limit={Telescope.settings.get("postsPerPage", 10)}
+            />
+          </div>
+
+          <div className="col-sm-6">
+            <Telescope.components.IssuesStoriesHeader type="Grievances"/>
+            <ListContainer
+              collection={Posts}
+              publication="posts.list"
+              selector={grievancesSelector}
+              options={options}
+              terms={params}
+              joins={Posts.getJoins()}
+              component={Telescope.components.IssuesStoriesList}
+              cacheSubscription={true}
+              listId="grievance"
+              limit={Telescope.settings.get("postsPerPage", 10)}
+            />
+          </div>
+
         </div>
-
-        <div className="col-sm-6">
-          <Telescope.components.IssuesStoriesHeader type="Grievances"/>
-          <ListContainer
-            collection={Posts}
-            publication="posts.list"
-            selector={grievancesSelector}
-            options={options}
-            terms={params}
-            joins={Posts.getJoins()}
-            component={Telescope.components.IssuesStoriesList}
-            cacheSubscription={true}
-            listId="grievance"
-            limit={Telescope.settings.get("postsPerPage", 10)}
-          />
-        </div>
-
       </div>
 
       <Telescope.components.PostsCommentsThread document={post} />
