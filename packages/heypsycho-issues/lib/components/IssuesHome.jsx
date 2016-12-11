8ed098b6101b09eb9ccf29221c72a7eb498b54d9
Jonathan McCoy <modelsolutions@gmail.com>
2016-12-11 12:53:41 -0800

    filtering for only 'issues' on front page

--- packages/heypsycho-issues/lib/components/IssuesHome.jsx
+++ packages/heypsycho-issues/lib/components/IssuesHome.jsx
@@ -2,14 +2,18 @@ import Telescope from 'meteor/nova:lib';
 import React, { PropTypes, Component } from 'react';
 import { ListContainer /* , DocumentContainer */ } from "meteor/utilities:react-list-container";
 import Posts from "meteor/nova:posts";
+import Categories from "meteor/nova:categories";
 
 class IssuesHome extends Telescope.components.PostsHome {
 
   render() {
-
+    
+    const issuesCategoryId = Categories.findOne({name: 'Issues'})._id;
+    
     const params = {...this.getDefaultView(), ...this.props.location.query, listId: "posts.list.main"};
     const {selector, options} = Posts.parameters.get(params);
-
+    selector['categories'] = issuesCategoryId;
+    
     return (
       <ListContainer
         collection={Posts}
