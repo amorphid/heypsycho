722baf5aa80754f69ab1ce7a4575f49f753ae832
Jonathan McCoy <modelsolutions@gmail.com>
2016-12-10 18:10:28 -0800

    setting primary route to issues

--- packages/my-custom-package/lib/routes.jsx
+++ packages/my-custom-package/lib/routes.jsx
@@ -6,4 +6,7 @@ Browse to http://localhost:3000/my-custom-route to see it.
 import Telescope from 'meteor/nova:lib';
 import CustomPostsHome from './components/CustomPostsHome.jsx';
 
-Telescope.routes.add({name:"issues", path:"/issues", component: CustomPostsHome});
+Telescope.routes.indexRoute = { 
+  name: "issues", 
+  component: CustomPostsHome
+};
