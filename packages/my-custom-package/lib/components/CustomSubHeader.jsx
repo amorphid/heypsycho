37beb9f22e7d91057d3027790a2b22e8905d40a2
jake <jcbflmng@gmail.com>
2016-12-10 17:49:38 -0800

    Restructuring subheader and adding stylesheet structure

--- packages/my-custom-package/lib/components/CustomSubHeader.jsx
+++ packages/my-custom-package/lib/components/CustomSubHeader.jsx
@@ -2,7 +2,7 @@ import Telescope from 'meteor/nova:lib';
 import React, { PropTypes, Component } from 'react';
 
 const CustomSubHeader = (props, {currentUser}) => {
-  
+
   const logoUrl = Telescope.settings.get("logoUrl");
   const siteTitle = Telescope.settings.get("title", "Nova");
   const tagline = Telescope.settings.get("tagline");
@@ -10,9 +10,13 @@ const CustomSubHeader = (props, {currentUser}) => {
   return (
     <nav className="sub-header">
 
-      <div className="logo">
-        <Telescope.components.Logo siteTitle={siteTitle} />
-        {tagline ? <h2 className="tagline">{tagline}</h2> : "" }
+      <div className="row center-xs">
+        <div className="col-sm-8">
+          <Telescope.components.Logo siteTitle={siteTitle} />
+        </div>
+        <div className="col-sm-8">
+          {tagline ? <h2 className="tagline">{tagline}</h2> : "" }
+        </div>
       </div>
 
     </nav>
